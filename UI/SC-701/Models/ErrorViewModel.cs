52dc05058ff10c156fd20c0b2d2e91ca5bddbfa2
Brandon Vargas Moreira <136260759+Vargas0421@users.noreply.github.com>
2026-02-02 21:36:22 -0600

    creacion del proyecto

---
+++ UI/SC-701/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SC_701.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
